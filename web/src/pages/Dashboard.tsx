c147a99da308449265f8c4e0e9a676bd5370e0eb
Tun1n <slpedro21@outlook.com>
2026-08-20 22:20:22 -0300

    Criando a página de Dashboard e variações do botão

---
+++ web/src/pages/Dashboard.tsx
@@ -0,0 +1,33 @@
+import { useState } from "react";
+import { Input } from "../components/Input";
+import { Button } from "../components/Button";
+import searchSvg from  "../assets/search.svg"
+
+
+export function Dashboard(){
+    const [name, setName] = useState("")
+
+    function fetchRefunds(event: React.FormEvent){
+        event.preventDefault()
+        console.log(name)
+    }
+
+    return <>
+        <div className="bg-gray-500 rounded-xl p-10 md:min-w-[768px]">
+            <h1 className="text-gray-100 font-bold text-xl flex-1"> Solicitações </h1>
+
+            <form onSubmit={fetchRefunds} className="flex flex-1 items-center justify-between pb-6 border-b-[1px] 
+            border-b-gray-400 md:flex-row gap-2 mt-6">
+
+                <Input 
+                    placeholder="Pesquisar pelo nome"
+                    onChange={(event) => setName(event.target.value)}/>
+
+                <Button variant="icon"> 
+                    <img src={searchSvg} alt="Ícone de pesquisa" className="w-5" />
+                </Button>
+
+            </form>
+        </div>
+    </>
+}
--- web/src/routes/index.tsx
+++ web/src/routes/index.tsx
@@ -1,11 +1,10 @@
 import { BrowserRouter } from "react-router"; 
-import { AuthRoutes } from "./Auth-routes";
-import { EmployeeRoutes } from "./EmployeeRoutes";
+import { ManagerRoutes } from "./ManagerRoutes";
 
 export function Routes(){
     return <>
         <BrowserRouter>
-            <EmployeeRoutes/>
+            <ManagerRoutes/>
         </BrowserRouter>
     </>
 }
